6e4011c5f99f2284bca9d42fab85eee49a7f982a
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-12-08 14:19:11 +0100

    Composant RatingStars

---
+++ src/components/RatingStars.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="rating-stars">
+    <Star
+      v-for="i in 5"
+      :key="i"
+      :size="24"
+      :weight="i <= rating ? 'fill' : 'regular'"
+      @click="$emit('update:rating', i)"
+      class="cursor-pointer"
+    />
+  </div>
+</template>
+
+<script setup>
+    import { Star } from '@phosphor-icons/vue'
+    defineProps({
+        rating: Number,
+    })
+</script>
+
+<style>
+    .cursor-pointer { cursor: pointer; }
+</style>
